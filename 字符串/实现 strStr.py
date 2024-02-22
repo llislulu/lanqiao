ebc2156358ffefead0b88462d9ecff15f4a9229c
lu <1678870242@qq,com>
2024-02-22 20:48:09 +0800

    Initial commit

--- 字符串/实现 strStr.py
+++ 字符串/实现 strStr.py
@@ -11,9 +11,32 @@
 #         return -1
 
 class Solution:
+    def getnext(self, next, s) -> None:
+        j = 0
+        next[0] = 0
+        for i in range(1, len(s)):
+            while j >= 1 and s[i] != s[j]:
+                j = next[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            next[i] = j
+
     def strStr(self, haystack: str, needle: str) -> int:
-#         KMP算法
+        #         KMP算法
+        if not needle:
+            return 0
+        next = [0] * len(needle)
+        self.getnext(next, needle)
+        j = 0
+        for i in range(len(haystack)):
+            while j >= 1 and haystack[i] != needle[j]:
+                j = next[j-1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i - len(needle) + 1
+        return - 1
 
 
 solution = Solution()
-print(solution.strStr( "sadbutsad", "sad"))
+print(solution.strStr("adbutsad", "sad"))
